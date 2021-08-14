61bcf79f48e8f65df434dc08c19a6ce420bcb9e6
anbreaker <antunez19@gmail.com>
2021-08-14 12:48:16 +0200

    up project to component and comunication/pagination...

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,87 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <div class="row">
+      <h1>Coin Market</h1>
+
+      <input
+        type="text"
+        class="form-control text-light bg-dark rounded-0 border-0 my-4"
+        placeholder="Search Coin"
+        v-model="textSearch"
+        @keyup="searchCoin()"
+        autofocus
+      />
+
+      <table class="table table-hover table-dark text-light">
+        <thead>
+          <tr>
+            <th v-for="(title, index) in titles" :key="index">
+              {{ title }}
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="(coin, index) in filteredCoins" :key="coin.id">
+            <td class="text-muted">{{ index }}</td>
+            <td>
+              <img
+                :src="coin.image"
+                :alt="coin.name"
+                style="width: 2rem"
+                class="me-2"
+              />
+              <span>
+                {{ coin.name }}
+              </span>
+              <span class="ms-2 text-muted text-uppercase">
+                {{ coin.symbol }}
+              </span>
+            </td>
+            <td>{{ coin.current_price.toLocaleString() }}</td>
+            <td
+              :class="[
+                coin.price_change_percentage_24h > 0
+                  ? 'text-success'
+                  : 'text-danger',
+              ]"
+            >
+              {{ coin.price_change_percentage_24h }}
+            </td>
+            <td>{{ coin.total_volume.toLocaleString() }}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
+  data() {
+    return {
+      coins: [],
+      filteredCoins: [],
+      titles: ["#", "Coin", "Price", "Price Change", "24h Volume"],
+      textSearch: "",
+    };
+  },
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  async mounted() {
+    const response = await fetch(process.env.VUE_APP_API);
+
+    const data = await response.json();
+    this.coins = data;
+    this.filteredCoins = data;
+  },
+
+  methods: {
+    searchCoin() {
+      this.filteredCoins = this.coins.filter(
+        (coin) =>
+          coin.name.toLowerCase().includes(this.textSearch.toLowerCase()) ||
+          coin.symbol.toLowerCase().includes(this.textSearch.toLowerCase())
+      );
+    },
+  },
+};
+</script>
---
+++ src/components/Search.vue
@@ -0,0 +1,36 @@
+<template>
+  <input
+    type="text"
+    class="form-control text-light bg-dark rounded-0 border-0 my-4"
+    placeholder="Search Coin"
+    v-model="textSearch"
+    @keyup="searchCoin()"
+    autofocus
+  />
+</template>
+
+<script>
+export default {
+  name: "Search",
+
+  props: ["coins"],
+
+  data() {
+    return {
+      filteredCoins: [],
+      textSearch: "",
+    };
+  },
+
+  methods: {
+    searchCoin() {
+      this.filteredCoins = this.coins.filter(
+        (coin) =>
+          coin.name.toLowerCase().includes(this.textSearch.toLowerCase()) ||
+          coin.symbol.toLowerCase().includes(this.textSearch.toLowerCase())
+      );
+    },
+  },
+};
+</script>
+
